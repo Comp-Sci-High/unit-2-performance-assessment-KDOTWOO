66cc4d201cd8c8d22f48b49ff1b7da0e7c5804c3
KDOTWOO <Kemil.penapolanco26@compscihigh.org>
2025-11-19 05:12:18 +0000

    Finished

--- api.js
+++ api.js
@@ -5,3 +5,26 @@ const apiKey = process.env.apiKey2
 // run your code with node api.js
 // add your apiKey to .env if your API needs one, otherwise ignore
 // Follow the checklist on the Performance Assessment Google Doc
+let APIurl="https://openapi-test01.sheincorp.cn/open-api/openapi-business-backend/product";
+let input2=prompt("Brokie what you want:");
+async function isEditable() {
+    const options = {
+        method: 'POST',
+        headers: {
+            Authorization: 'Bearer ' + apiKey,
+            "Content-Type": 'application/json'
+
+        },
+        body: JSON.stringify({
+            pageNum: 1,
+            pageSize: 50,
+            input:input2
+
+        })
+    };
+    const response = await fetch(APIurl, options);
+
+    const data = await response.json();
+    console.log(info.data[2]);
+    return data;
+};
--- chatgpt.js
+++ chatgpt.js
@@ -5,3 +5,25 @@ const apiKey = process.env.apiKey1
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+let AIUrl = "https://api.openai.com/v1/responses";
+let input1 = prompt("What you need Bro ");
+async function basicText() {
+    const options = {
+        method: 'POST',
+        headers: {
+            Authorization: 'Bearer ' + apiKey,
+            "Content-Type": 'application/json'
+
+        },
+        body: JSON.stringify({
+            model: "gpt-4.1",
+            input: input1
+        })
+    };
+    const response = await fetch(AIUrl, options);
+
+    const data = await response.json();
+    console.log(data.output[0].content[0].text);
+    return data;
+};
+basicText()
